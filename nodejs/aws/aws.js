3f020f5093b84f481115c0b9bd41dfb23f34ca59
Juraj Kojdjak <juraj.kojdjak@gmail.com>
2016-03-11 14:28:18 +0000

    my local aws lib/module

--- nodejs/aws/aws.js
+++ nodejs/aws/aws.js
@@ -1,1 +1,23 @@
 var aws = require('aws-sdk');
+
+//change to config.json
+aws.config.loadFromPath('/home/centos/production/homepage/nodejs/aws/config/config2.json');
+
+var ec2 = new aws.EC2();
+
+module.exports = {
+    ec2: ec2,
+    listInstances: function(callback) {
+        ec2.describeInstances({}, function(err, data) {
+            if (err) {
+                callback(err);
+            }
+            else {
+                callback(null, data.Reservations[0].Instances);
+            }
+
+        });
+    }
+};
+
+
