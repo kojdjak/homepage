39cfbb8bb092df5ee682991241b23f7b24ad7707
Juraj Kojdjak <juraj.kojdjak@gmail.com>
2016-03-14 16:43:53 +0000

    initially add react application

---
+++ reactjs/app/app.js
@@ -0,0 +1,14 @@
+var App = React.createClass({
+    render: function() {
+        return (
+            <div>
+                Hello World
+            </div>
+        );
+    }
+});
+ReactDOM.render(
+    <App />,
+    document.getElementById('content')
+);
+
