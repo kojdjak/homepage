9d4eb56176bfa160b7259aad4c4510080f47f2b8
Juraj Kojdjak <juraj.kojdjak@gmail.com>
2016-03-14 17:10:48 +0000

    create skeleton of simple list of VMs

--- reactjs/app/app.js
+++ reactjs/app/app.js
@@ -1,12 +1,45 @@
+var VM = React.createClass({
+    render: function() {
+        return (
+            <div>
+                {this.props.name}
+            </div>
+        );
+    }
+});
+
+var ListVMs = React.createClass({
+    render: function() {
+        var vms = this.props.data.map(function(vm) {
+            return (
+                <VM name={vm.name} key={vm.name} />
+            );
+        });
+        console.log(vms);
+        return ( 
+            <div>
+            {vms}
+            </div>
+        );
+    }
+});
+
+var data = [
+  { name: "VM1" },
+  { name: "VM2" }
+]
+
 var App = React.createClass({
     render: function() {
         return (
             <div>
                 Hello World
+                <ListVMs data={data} />
             </div>
         );
     }
 });
+
 ReactDOM.render(
     <App />,
     document.getElementById('content')
