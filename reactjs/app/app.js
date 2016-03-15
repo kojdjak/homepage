2516f58675c86e8c495f1d983bd703a8c7712d75
Juraj Kojdjak <juraj.kojdjak@gmail.com>
2016-03-15 20:07:23 +0000

    load data from my aggregator service

--- reactjs/app/app.js
+++ reactjs/app/app.js
@@ -9,39 +9,54 @@ var VM = React.createClass({
 });
 
 var ListVMs = React.createClass({
-    render: function() {
-        var vms = this.props.data.map(function(vm) {
+   render: function() {
+        console.log(typeof this.props.data);
+            var vms = this.props.data.map(function(vm) {
             return (
                 <VM name={vm.name} key={vm.name} />
             );
         });
-        console.log(vms);
         return ( 
             <div>
             {vms}
             </div>
+        
         );
     }
 });
 
-var data = [
-  { name: "VM1" },
-  { name: "VM2" }
-]
-
 var App = React.createClass({
+    getInitialState: function() {
+        return {data: []};
+    },
+    componentDidMount: function() {
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            cache: false,
+            success: function(data) {
+                this.setState({data: data});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+    componentWillUnmount: function() {
+              this.serverRequest.abort();
+                },
     render: function() {
         return (
             <div>
-                Hello World
-                <ListVMs data={data} />
+                VMs at amazon:
+                <ListVMs data={this.state.data}/>
             </div>
         );
     }
 });
 
 ReactDOM.render(
-    <App />,
+    <App url="/homepage/aws/aws3"/>,
     document.getElementById('content')
 );
 
